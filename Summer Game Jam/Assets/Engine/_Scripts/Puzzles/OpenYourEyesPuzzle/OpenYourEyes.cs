32140ffadf4e9a1578cf3f1c4be60dcafe77e516
Adam Eustis <eust0015@flinders.edu.au>
2026-02-18 12:16:28 +1030

    OpenYourEyes inherits Puzzle & added input prompt

--- Summer Game Jam/Assets/Engine/_Scripts/Puzzles/OpenYourEyesPuzzle/OpenYourEyes.cs
+++ Summer Game Jam/Assets/Engine/_Scripts/Puzzles/OpenYourEyesPuzzle/OpenYourEyes.cs
@@ -1,21 +1,25 @@
 using UnityEngine;
 using UnityEngine.Events;
 
-public class OpenYourEyes : MonoBehaviour
+public class OpenYourEyes : Puzzle
 {
     [SerializeField] private RectTransform topEyeLid;
     [SerializeField] private RectTransform bottomEyeLid;
     [SerializeField] private GameObject innerMonologue;
+    [SerializeField] private GameObject inputPrompt;
     [SerializeField] private UnityEvent onPuzzleCompleted;
     private const float speedInPixelsPerSecond = 1f;
+    private const float durationInSeconds = 3f;
     private static readonly Vector2 topEyeLidClosedPosition = new(0, 270f);
     private static readonly Vector2 topEyeLidOpenPosition = new(0, 500f);
     private static readonly Vector2 bottomEyeLidClosedPosition = new(0, -270f);
     private static readonly Vector2 bottomEyeLidOpenPosition = new(0, -500f);
+    private Vector2 topEyeLidVelocity;
+    private Vector2 bottomEyeLidVelocity;
 
     private void Update()
     {
-        if (Input.GetMouseButton(0))
+        if (Input.GetKey(KeyCode.F) || Input.GetMouseButton(0))
         {
             OpenEyes();
         }
@@ -27,11 +31,11 @@ public class OpenYourEyes : MonoBehaviour
 
     private void OpenEyes()
     {
-        topEyeLid.anchoredPosition = Vector2.Lerp(topEyeLid.anchoredPosition, topEyeLidOpenPosition, Time.deltaTime * speedInPixelsPerSecond);
-        bottomEyeLid.anchoredPosition = Vector2.Lerp(bottomEyeLid.anchoredPosition, bottomEyeLidOpenPosition, Time.deltaTime * speedInPixelsPerSecond);
+        topEyeLid.anchoredPosition = Vector2.SmoothDamp(topEyeLid.anchoredPosition, topEyeLidOpenPosition, ref topEyeLidVelocity, durationInSeconds);
+        bottomEyeLid.anchoredPosition = Vector2.SmoothDamp(bottomEyeLid.anchoredPosition, bottomEyeLidOpenPosition, ref bottomEyeLidVelocity, durationInSeconds);
         innerMonologue.SetActive(false);
 
-        if (Vector2.Distance(topEyeLid.anchoredPosition, topEyeLidOpenPosition) < 0.1f)
+        if (Vector2.Distance(topEyeLid.anchoredPosition, topEyeLidOpenPosition) < 0.5f)
         {
             PuzzleCompleted();
         }
@@ -39,14 +43,28 @@ public class OpenYourEyes : MonoBehaviour
 
     private void CloseEyes()
     {
-        topEyeLid.anchoredPosition = Vector2.Lerp(topEyeLid.anchoredPosition, topEyeLidClosedPosition, Time.deltaTime * speedInPixelsPerSecond);
-        bottomEyeLid.anchoredPosition = Vector2.Lerp(bottomEyeLid.anchoredPosition, bottomEyeLidClosedPosition, Time.deltaTime * speedInPixelsPerSecond);
+        topEyeLid.anchoredPosition = Vector2.SmoothDamp(topEyeLid.anchoredPosition, topEyeLidClosedPosition, ref topEyeLidVelocity, durationInSeconds);
+        bottomEyeLid.anchoredPosition = Vector2.SmoothDamp(bottomEyeLid.anchoredPosition, bottomEyeLidClosedPosition, ref bottomEyeLidVelocity, durationInSeconds);
         innerMonologue.SetActive(true);
+    }
+
+    public override void ActivatePuzzle()
+    {
+        base.ActivatePuzzle();
+        topEyeLid.anchoredPosition = topEyeLidClosedPosition;
+        bottomEyeLid.anchoredPosition = bottomEyeLidClosedPosition;
+        innerMonologue.SetActive(true);
+        inputPrompt.SetActive(true);
+        enabled = true;
     }
 
     private void PuzzleCompleted()
     {
         onPuzzleCompleted?.Invoke();
         enabled = false;
+        topEyeLid.gameObject.SetActive(false);
+        bottomEyeLid.gameObject.SetActive(false);
+        inputPrompt.SetActive(false);
+        SolvePuzzle();
     }
 }
